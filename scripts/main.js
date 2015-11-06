6e5ef0d47b43ca245e9ba8792b3114c737a0ae69
zhangdi <zhangdi1@beisen.com>
2015-11-06 15:38:43 +0800

    add files

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,23 @@
 requirejs.config({
-	paths : {
-		"$" : "../bower_components/jquery/dist/jquery",
-		"React" : "../bower_components/react/react",
-		"ReactDOM" : "../bower_components/react-dom/react-dom",
+	paths: {
+		"$": "../bower_components/jquery/dist/jquery",
+		"React": "../bower_components/react/react",
+		"ReactDOM": "../bower_components/react-dom/react-dom",
 		"JSX": "../bower_components/requirejs-react-jsx/jsx",
-    	"text": "../bower_components/requirejs-text/text"
+		"text": "../bower_components/requirejs-text/text"
 	}
 });
 
 // 自己写的略low的路由
-var hash = window.location.hash;
-hash = hash.substr(1);
-require(["js/"+ hash +"-view"]);
-// 监听的并没有什么卵用
-window.addEventListener("hashchange",function(hash){
-	console.log(hash)
+function renderToContent() {
+	var hash = window.location.hash;
+	hash = hash.substr(1);
+	require(["js/" + hash + "-view"]);
+}
+
+window.addEventListener("hashchange", function(obj) {
+	$("#content_wrap").innerHTML = "";
+	renderToContent();
 });
+
+renderToContent();
